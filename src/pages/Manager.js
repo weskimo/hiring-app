7730f419bd39279d1a002cab0f80d4ffd2002b20
Weskimo <samuel.hughes4@btinternet.com>
2022-05-26 15:57:19 +0100

    added view and styls for the manager page. Using accordian to add the tasks through a list

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -4,6 +4,10 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Typography from '@mui/material/Typography';
+
 
 export default function Manager() {
 
@@ -30,9 +34,76 @@ export default function Manager() {
      } 
 
     return(
-        <div>
-                <h1>Manager</h1>
-            </div>
+        <>
+
+        <h1> Add Jobs Here:</h1>
+        <div className='add-Job'>
+        </div>
+       
+       
+        <div className='add-Job'>
+        <input
+            type="text"
+            placeholder="Job Name"
+            onChange={ (event) => setNewJobName(event.target.value) }
+            value={newJobName}
+            maxLength="40"
+        />
+        <input
+
+            type="text"
+            placeholder="Monetary Compensation"
+            onChange={ (event) => setNewJobMonComp(event.target.value) }
+            value={newJobMonComp}
+            maxLength="40"
+        />
+        <input
+
+            type="text"
+            placeholder="Non-Monetary Compensation"
+            onChange={ (event) => setNewJobNonMonComp(event.target.value) }
+            value={newJobNonMonComp}
+            maxLength="100"
+        />
+        <button onClick={addItem}>Add Item</button>
+        </div>
+
+
+
+
+        <List children={list} sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {list.map((item) =>
+                <ListItem alignItems="center">
+                     <Accordion expanded={expanded === list.indexOf(item)} onChange={handleChange(list.indexOf(item))}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                    >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Job Title:
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}> {item.name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        Salary: {item.monComp}
+                    </Typography>
+                    <Typography>
+                        Other: {item.nonMonComp}
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+
+                </ListItem>
+
+
+            )}
+        </List>
+
+
+    </>
     )
       
 }
