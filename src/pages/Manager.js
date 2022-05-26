5382e9408f5ebc14da49a9869c8996dfc55c8dfc
Weskimo <samuel.hughes4@btinternet.com>
2022-05-26 15:32:53 +0100

    added use state to the manager page, going to use accordian to solve this page

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,4 +1,33 @@
+import * as React from 'react';
+import { useState } from "react"
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export default function Manager() {
+
+
+    const [list, setList] = useState([])
+
+    const [newJobName, setNewJobName] = useState('')
+    const [newJobMonComp, setNewJobMonComp] = useState('')
+    const [newJobNonMonComp, setNewJobNonMonComp] = useState('')
+
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    function addItem(e) {
+        setList(prevList => {
+            return [...prevList, {name: newJobName, monComp: newJobMonComp, nonMonComp: newJobNonMonComp}]
+        })
+        setNewJobMonComp('')
+        setNewJobName('')
+        setNewJobNonMonComp('')
+     } 
 
     return(
         <div>
